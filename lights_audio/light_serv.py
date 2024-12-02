841df38e9cf77620d5aa30fb25232c79a71fbc36
Efren Haskell <efrenhask@gmail.com>
2024-12-02 18:49:49 -0500

    Light server code

--- lights_audio/light_serv.py
+++ lights_audio/light_serv.py
@@ -11,26 +11,28 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 sock.bind(("", 1234))
 sock.listen(5)
+print("server started on port 1234")
+
 color: str = "blue"
 rec_flag: bool = False
 sens_flag: bool = False
 light_flag: bool = False
-clients: list = [None]*3
+clients: list = []
+reciever = None
 while not rec_flag and not (rec_flag == sens_flag == light_flag):
     # Establish connection with client.
     c, addr = sock.accept()
     handshake: str = c.recv(1024).decode()
     if handshake == "face_rec":
         rec_flag = True
-        clients[0] = c
+        clients.append(c)
     elif handshake == "motion_sensor":
         sens_flag = True
-        clients[1] = c
+        clients.append(c)
     elif handshake == "light_system":
         light_flag = True
-        clients[2] = c
+        reciever = c
     print('Got connection from', addr)
 while True:
-    check_recv(clients[0].recv(1024).decode(), clients[2])
-    check_recv(clients[1].recv(1024).decode(), clients[2])
-
+    for client in clients:
+        check_recv(client.recv(1024).decode(), reciever)
