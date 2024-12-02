1179a4937078bd98e1add10cf469f919c96687dc
Efren Haskell <efrenhask@gmail.com>
2024-12-02 18:42:40 -0500

    Light receiver code

---
+++ lights_audio/light_recv.py
@@ -0,0 +1,22 @@
+import socket
+import machine
+import neopixel
+
+n = 60  # Number of LEDs
+pin = machine.Pin(1)
+np = neopixel.NeoPixel(pin, n)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+color_codes: dict[str, tuple[int, int, int]] = {"red": (255, 0, 0), "white": (255, 255, 255), "green": (0, 255, 0), "blue": (0, 0, 255)}
+
+for i in range(n):
+    np[i] = (255, 255, 255)
+
+
+client.connect(("172.20.164.94", 1234))
+while True:
+    color = client.recv(1024).decode()
+    if color is not None:
+        for i in range(n):
+            np[i] = color_codes[color]
